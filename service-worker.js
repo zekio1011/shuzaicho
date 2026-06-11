6bd0190b21f4a7ad719351f8c58d9c47e38d5368
zekio1011 <amane.kojima1011@gmail.com>
2026-06-11 16:48:17 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,14 @@
-const CACHE_NAME = "shuzaicho-v2";
+const CACHE_NAME = "shuzaicho-v3";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
+  );
+  self.clients.claim();
 });
 self.addEventListener("fetch", event => {
   event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
